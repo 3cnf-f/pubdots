41e9912203be91e09f34fbdb981a9591643844d1
3cnf-f <154888836+3cnf-f@users.noreply.github.com>
2024-09-05 12:01:44 +0200

    Update install-gh-cli.sh

--- install-gh-cli.sh
+++ install-gh-cli.sh
@@ -1,5 +1,5 @@
 apt update \
-&& apt upgrade -y
+&& apt upgrade -y \
 && apt -y install wget \
 && mkdir -p -m 755 /etc/apt/keyrings \
 && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
